d6c44d83b90c912c5cc6f46f7a90a7bedea3a5a7
Kautenja <kautencreations@gmail.com>
2018-09-10 17:22:31 -0400

    update plot method

--- src/camvid/visualize.py
+++ src/camvid/visualize.py
@@ -2,19 +2,27 @@
 from matplotlib import pyplot as plt
 
 
-def plot(**kwargs: dict) -> None:
+# the DPI for CamVid images is 72.0
+DPI = 36.0
+
+
+def plot(dpi: float=96.0, **kwargs: dict) -> None:
     """
     Plot the original image, the true y, and an optional predicted y.
 
     Args:
+        dpi: the DPI of the figure to render
         kwargs: images to plot
 
     Returns:
         None
 
     """
+    # determine the image shape for platting based on DPI
+    image_shape = list(kwargs.values())[0].shape
+    figsize = image_shape[0] / DPI, image_shape[1] / DPI
     # create subplots for each image
-    _, axarr = plt.subplots(1, len(kwargs))
+    _, axarr = plt.subplots(len(kwargs), 1, figsize=figsize, dpi=dpi)
     # iterate over the images in the dictionary
     for idx, (title, img) in enumerate(kwargs.items()):
         # plot the image
