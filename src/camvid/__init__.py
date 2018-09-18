176efc906ef7cbfbab5a8c8c52ba93c9e5c59aa5
Kautenja <kautencreations@gmail.com>
2018-09-18 12:47:45 -0500

    rename visualize to plot

--- src/camvid/__init__.py
+++ src/camvid/__init__.py
@@ -1,7 +1,7 @@
 """This package provides methods to load the CamVid dataset into memory."""
 from .camvid import CamVid
 from .create_segmented_y import create_segmented_y
-from .visualize import plot
+from .plot import plot
 
 
 # explicitly define the outward facing API of this package
