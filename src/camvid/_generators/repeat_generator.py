2124593853817756a40230f41673f1755fed9488
Kautenja <kautencreations@gmail.com>
2018-09-18 12:37:15 -0500

    refactor

--- src/camvid/_generators/repeat_generator.py
+++ src/camvid/_generators/repeat_generator.py
@@ -1,23 +1,39 @@
 """A method to repeat a generators output for multi-IO models."""
 
 
-def repeat_generator(x, y, x_repeats=0, y_repeats=0):
+def repeat_generator(
+    x_gen: 'DataGenerator',
+    y_gen: 'DataGenerator',
+    x_repeats: int=0,
+    y_repeats: int=0
+) -> 'DataGenerator':
     """
     Return a generator that repeats x and y input generators.
 
     Args:
-        x: a generator for x data
-        y: a generator for y data
+        x_gen: a directory for x data
+        y_gen: a generator for y data
         x_repeats: the number of times to repeat the x data (default 0)
         y_repeats: the number of times to repeat the y data (default 0)
 
     Returns:
-        a new generator that returns a tuple of lists size (x_repeats + 1) and
-        (y_repeats + 1) respectively
+        a new generator that returns a tuple of (X, y) lists with sizes:
+        - (x_repeats + 1)
+        - (y_repeats + 1)
 
     """
     # create a mapping function to repeat the x and y inputs
     def repeat_outputs(_x, _y):
+        """
+        Repeat the generator outputs of input generators.
+
+        Args:
+            _x: the x generator to repeat
+            _y: the y generator to repeat
+
+        Returns: a list with inputs repeated x_repeats and y_repeats times
+
+        """
         # repeat if there is a positive repeat rate
         if x_repeats > 0:
             _x = [_x] * (x_repeats + 1)
@@ -27,7 +43,7 @@ def repeat_generator(x, y, x_repeats=0, y_repeats=0):
         # return the updated x and y values
         return _x, _y
     # return the new mapping for the generators
-    return map(repeat_outputs, x, y)
+    return map(repeat_outputs, x_gen, y_gen)
 
 
 # explicitly define the outward facing API of this module
