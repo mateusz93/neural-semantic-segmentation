4fd544c75f8afd6736e6e7b4ca5b3719eabcfa90
Kautenja <kautencreations@gmail.com>
2018-09-19 12:07:03 -0500

    unpool method;

--- src/backend/tensorflow_backend.py
+++ src/backend/tensorflow_backend.py
@@ -1,8 +1,8 @@
 """Extensions to the TensorFlow backend for Keras."""
+from keras import backend as K
 from keras.backend.tensorflow_backend import tf
 from keras.backend.tensorflow_backend import _preprocess_conv2d_input
 from keras.backend.tensorflow_backend import _preprocess_padding
-from keras.backend.common import normalize_data_format
 
 
 def pool2d_argmax(x, pool_size,
@@ -29,7 +29,7 @@ def pool2d_argmax(x, pool_size,
         ValueError: if `pool_mode` is neither `"max"` or `"avg"`.
 
     """
-    data_format = normalize_data_format(data_format)
+    data_format = K.common.normalize_data_format(data_format)
 
     x, tf_data_format = _preprocess_conv2d_input(x, data_format)
     padding = _preprocess_padding(padding)
@@ -43,8 +43,7 @@ def pool2d_argmax(x, pool_size,
     if pool_mode == 'max':
         x, idx = tf.nn.max_pool_with_argmax(x, pool_size, strides, padding=padding)
     elif pool_mode == 'avg':
-        # TODO: implement or find implementation for average pooling with index
-        raise NotImplementedError('TensorFlow is missing the required method')
+        raise NotImplementedError('no support for avg pooling with index')
     else:
         raise ValueError('Invalid pool_mode: ' + str(pool_mode))
 
@@ -55,5 +54,49 @@ def pool2d_argmax(x, pool_size,
     return x, idx
 
 
+def unpool2d_argmax(x, idx, pool_size):
+    """
+    Un-pooling layer after pool2d_argmax.
+
+    Args:
+        x: Tensor or variable
+        idx: index matching the shape of x
+        pool_size: the pool_size used by the pooling operation
+
+    Returns:
+        an un-pooled version of x using indexes in idx
+
+    Notes:
+        follow the issue here for updates on this functionality:
+        https://github.com/tensorflow/tensorflow/issues/2169
+
+    """
+    in_s = K.shape(x)
+    out_s = [in_s[0], in_s[1] * pool_size[0], in_s[2] * pool_size[1], in_s[3]]
+
+    flat_input_size = K.prod(in_s)
+    flat_output_shape = [out_s[0], out_s[1] * out_s[2] * out_s[3]]
+
+    b_shape = [in_s[0], 1, 1, 1]
+    b_range = K.reshape(K.arange(K.cast(out_s[0], 'int64')), shape=b_shape)
+    b = K.ones_like(idx) * b_range
+    b1 = K.reshape(b, [flat_input_size, 1])
+    ind_ = K.reshape(idx, [flat_input_size, 1])
+    ind_ = K.concatenate([b1, ind_], 1)
+
+    pool = K.reshape(x, [flat_input_size])
+    ret = tf.scatter_nd(ind_, pool, shape=K.cast(flat_output_shape, 'int64'))
+    ret = K.reshape(ret, out_s)
+
+    in_s = K.int_shape(x)
+    ret_s = [in_s[0], in_s[1] * pool_size[0], in_s[2] * pool_size[1], in_s[3]]
+    ret.set_shape(ret_s)
+
+    return ret
+
+
 # explicitly define the outward facing API of this module
-__all__ = ['pool2d_argmax']
+__all__ = [
+    pool2d_argmax.__name__,
+    unpool2d_argmax.__name__,
+]
