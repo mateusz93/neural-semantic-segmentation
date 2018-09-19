446f2de0cf6d4d889bbebccc8b8c2858041d3098
Kautenja <kautencreations@gmail.com>
2018-09-19 18:00:57 -0500

    cleanup

--- src/losses/weighted_categorical_crossentropy.py
+++ src/losses/weighted_categorical_crossentropy.py
@@ -3,9 +3,9 @@ from ..backend.tensorflow_backend import categorical_crossentropy
 
 
 def build_weighted_categorical_crossentropy(weights):
-    def _categorical_crossentropy(y_true, y_pred):
+    def weighted_categorical_crossentropy(y_true, y_pred):
         return categorical_crossentropy(y_true, y_pred, weights)
-    return _categorical_crossentropy
+    return weighted_categorical_crossentropy
 
 
 # explicitly define the outward facing API of this module
