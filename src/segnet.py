a95e69360408f5a1f2831b73af27c47ff5ce0349
Kautenja <kautencreations@gmail.com>
2018-09-19 13:44:28 -0500

    update nomenclature to match paper

--- src/segnet.py
+++ src/segnet.py
@@ -37,12 +37,12 @@ def conv_bn_relu(x, num_filters: int):
     return x
 
 
-def downsample(x, num_conv: int, num_filters: int):
+def encode(x, num_conv: int, num_filters: int):
     """
-    Append a down-sampling block with a given size and number of filters.
+    Append a encoder block with a given size and number of filters.
 
     Args:
-        x: the input tensor to append this down-sample block to
+        x: the input tensor to append this encoder block to
         num_conv: the number of convolutional blocks to use before pooling
         num_filters: the number of filters in each convolutional layer
 
@@ -59,12 +59,12 @@ def downsample(x, num_conv: int, num_filters: int):
     return x, pool
 
 
-def upsample(x, pool: MemorizedMaxPooling2D, num_conv: int, num_filters: int):
+def decode(x, pool: MemorizedMaxPooling2D, num_conv: int, num_filters: int):
     """
-    Append an up-sampling block with a given size and number of filters.
+    Append a decoder block with a given size and number of filters.
 
     Args:
-        x: the input tensor to append this up-sample block to
+        x: the input tensor to append this decoder block to
         pool: the corresponding memorized pooling layer to reference indexes
         num_conv: the number of convolutional blocks to use before pooling
         num_filters: the number of filters in each convolutional layer
@@ -79,7 +79,7 @@ def upsample(x, pool: MemorizedMaxPooling2D, num_conv: int, num_filters: int):
     return x
 
 
-def classification(x, num_classes: int):
+def classify(x, num_classes: int):
     """
     Add a Softmax classification block to an input CNN.
 
@@ -107,7 +107,7 @@ def build_segnet(
     num_classes: int,
     label_names: dict=None,
     optimizer=SGD(lr=0.1, momentum=0.9),
-    transfer_imagenet: bool=True,
+    pretrain_encoder: bool=True,
 ) -> Model:
     """
     Build a SegNet model for the given image shape.
@@ -117,7 +117,7 @@ def build_segnet(
         num_classes: the number of classes to segment for (e.g. c)
         label_names: a dictionary mapping discrete labels to names for IoU
         optimizer: the optimizer for training the network
-        transfer_imagenet: whether to initialize the down-sampler from VGG16
+        pretrain_encoder: whether to initialize the encoder from VGG16
 
     Returns:
         a Keras model of the 103 layer Tiramisu version of DenseNet
@@ -127,20 +127,20 @@ def build_segnet(
     inputs = Input(image_shape)
     # assume 8-bit inputs and convert to floats in [0,1]
     x = Lambda(lambda x: x / 255.0)(inputs)
-    # down-sample
-    x, p1 = downsample(x, num_conv=2, num_filters=64)
-    x, p2 = downsample(x, num_conv=2, num_filters=128)
-    x, p3 = downsample(x, num_conv=3, num_filters=256)
-    x, p4 = downsample(x, num_conv=3, num_filters=512)
-    x, p5 = downsample(x, num_conv=3, num_filters=512)
-    # up-sample
-    x = upsample(x, p5, num_conv=3, num_filters=512)
-    x = upsample(x, p4, num_conv=3, num_filters=256)
-    x = upsample(x, p3, num_conv=3, num_filters=128)
-    x = upsample(x, p2, num_conv=2, num_filters=64)
-    x = upsample(x, p1, num_conv=2, num_filters=64)
-    # classification
-    x = classification(x, num_classes)
+    # encoder
+    x, p1 = encode(x, num_conv=2, num_filters=64)
+    x, p2 = encode(x, num_conv=2, num_filters=128)
+    x, p3 = encode(x, num_conv=3, num_filters=256)
+    x, p4 = encode(x, num_conv=3, num_filters=512)
+    x, p5 = encode(x, num_conv=3, num_filters=512)
+    # decoder
+    x = decode(x, p5, num_conv=3, num_filters=512)
+    x = decode(x, p4, num_conv=3, num_filters=256)
+    x = decode(x, p3, num_conv=3, num_filters=128)
+    x = decode(x, p2, num_conv=2, num_filters=64)
+    x = decode(x, p1, num_conv=2, num_filters=64)
+    # classifier
+    x = classify(x, num_classes)
     # compile the graph
     model = Model(inputs=[inputs], outputs=[x])
     model.compile(
@@ -153,7 +153,7 @@ def build_segnet(
         ],
     )
     # if transfer learning from ImageNet is disabled, return the model as is
-    if not transfer_imagenet:
+    if not pretrain_encoder:
         return model
     # load the pre-trained VGG16 model using ImageNet weights
     vgg16 = VGG16(weights='imagenet', include_top=False)
