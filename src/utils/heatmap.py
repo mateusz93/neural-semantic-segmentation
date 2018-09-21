d1786b4c8810aa971bc9523425724fe4ed33ca0e
Kautenja <kautencreations@gmail.com>
2018-09-21 15:40:13 -0500

    customizable color space

--- src/utils/heatmap.py
+++ src/utils/heatmap.py
@@ -2,12 +2,13 @@
 from matplotlib import pyplot as plt
 
 
-def heatmap(arr):
+def heatmap(arr, cm='jet'):
     """
     Use the CMRmap color map to convert the input array to a heat-map.
 
     Args:
         arr: the array to convert to a heat-map
+        cm: the color map to use (defaults to 'jet')
 
     Returns:
         arr converted to a heat-map via the CMRmap color map
@@ -16,7 +17,7 @@ def heatmap(arr):
     # normalize the input data
     arr = plt.Normalize()(arr)
     # return the color map with the alpha channel omitted
-    heat = plt.cm.jet(arr)[..., :-1] * 255
+    heat = getattr(plt.cm, cm)(arr)[..., :-1] * 255
 
     return heat.astype('uint8')
 
