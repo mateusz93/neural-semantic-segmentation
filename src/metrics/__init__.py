07d9d5c705e79e8bfaa6d61029e5010aed5a432a
Kautenja <ckfour@icloud.com>
2018-09-20 23:44:42 -0500

    per class accuracy

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,12 +1,12 @@
 """Custom metrics for Keras."""
 from .iou import build_iou_for
 from .iou import mean_iou
-from .per_class_accuracy import build_per_class_accuracy
+from .per_class_accuracy import per_class_accuracy
 
 
 # explicitly define the outward facing API of this package
 __all__ = [
     build_iou_for.__name__,
-    build_per_class_accuracy.__name__,
-    mean_iou.__name__
+    mean_iou.__name__,
+    per_class_accuracy.__name__,
 ]
--- src/metrics/per_class_accuracy.py
+++ src/metrics/per_class_accuracy.py
@@ -1,45 +1,50 @@
-"""A Keras metric to calculate the per class accuracy given some weights."""
+"""A Keras metric to calculate the per class accuracy in categorical models."""
 from keras import backend as K
 
-# weights info: https://github.com/alexgkendall/caffe-segnet/issues/37
-def build_per_class_accuracy(weights=None):
+
+def class_accuracy(y_true, y_pred, label: int):
     """
-    Build a per class metric accuracy with input parameters.
+    Return the accuracy for a given label.
 
     Args:
-        weights: weights matching rank with predicted labels
+        y_true: the expected y values as a one-hot
+        y_pred: the predicted y values as a one-hot or Softmax output
+        c: the label to return the accuracy for
 
     Returns:
-        a callable Keras metric for per class accuracy
+        the accuracy for the given label
 
     """
-    if weights is not None:
-        if K.int_shape(weights) > 1:
-            weights = K.reshape(weights, -1)
-    # build the metric with the given parameters
-    def per_class_accuracy(y_true, y_pred):
-        """
-        Return the per class accuracy of predictions.
+    # extract the label values using the ArgMax operator then calculate
+    # equality of the predictions and truths to the label
+    y_true = K.equal(K.argmax(y_true), label)
+    y_pred = K.equal(K.argmax(y_pred), label)
+    # calculate the accuracy as average of correct predictions
+    return K.mean(K.cast(K.equal(y_true, y_pred), K.floatx()))
 
-        Args:
-            y_true: the ground truth labels
-            y_pred: the predicted labels
 
-        Returns:
-            the per class accuracy of y_pred based on y_true
+def per_class_accuracy(y_true, y_pred):
+    """
+    Return the per class accuracy of predictions.
 
-        """
-        # y_true = K.flatten(K.argmax(y_true, axis=-1))
-        # y_pred = K.flatten(K.argmax(y_pred, axis=-1))
-        # is_correct = K.cast(K.equal(y_true, y_pred), K.floatx())
+    Args:
+        y_true: the ground truth labels
+        y_pred: the predicted labels
 
-        # if weights is not None:
-        #     is_correct *= weights[y_true]
+    Returns:
+        the per class accuracy of y_pred based on y_true
 
-        # return K.mean(is_correct)
-
-    return per_class_accuracy
+    """
+    # get number of labels to calculate class accuracy for
+    num_labels = K.int_shape(y_pred)[-1]
+    # initialize a variable to store total accuracy in
+    acc = K.variable(0)
+    # iterate over labels to calculate accuracy for
+    for label in range(num_labels):
+        acc = acc + class_accuracy(y_true, y_pred, label)
+    # divide total acc by number of labels to get per class accuracy
+    return acc / num_labels
 
 
 # explicitly define the outward facing API of this module
-__all__ = [build_per_class_accuracy.__name__]
+__all__ = [per_class_accuracy.__name__]
