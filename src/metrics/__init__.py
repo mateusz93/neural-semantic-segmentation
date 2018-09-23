93a492acc7a94d2ed35a5fc680e719a877114929
Kautenja <kautencreations@gmail.com>
2018-09-23 14:44:41 -0500

    mean per class accuracy metric

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,10 +1,12 @@
 """Custom metrics for Keras."""
 from .iou import build_iou_for
 from .iou import mean_iou
+from .mean_per_class_accuracy import mean_per_class_accuracy
 
 
 # explicitly define the outward facing API of this package
 __all__ = [
     build_iou_for.__name__,
     mean_iou.__name__,
+    mean_per_class_accuracy.__name__,
 ]
