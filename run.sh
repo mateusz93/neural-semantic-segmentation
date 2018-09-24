383495ee25c0a40bf622096863c486b7674b4fa8
Kautenja <kautencreations@gmail.com>
2018-09-24 13:33:55 -0400

    explicitly select kernel for server usage

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p models
-python3 -m jupyter nbconvert --output-dir ./models --ExecutePreprocessor.timeout=-1 --to notebook --execute $1
+python3 -m jupyter nbconvert --output-dir ./models --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.kernel_name=sys_python36 --to notebook --execute $1
