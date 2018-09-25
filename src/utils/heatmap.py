0d7497a829ad4bff15ec28dc89ecb1b904592472
Kautenja <kautencreations@gmail.com>
2018-09-25 15:29:39 -0500

    better default color map

--- src/utils/heatmap.py
+++ src/utils/heatmap.py
@@ -2,13 +2,17 @@
 from matplotlib import pyplot as plt
 
 
-def heatmap(arr, cm='jet'):
+def heatmap(arr, color_map='cubehelix'):
     """
     Use the CMRmap color map to convert the input array to a heat-map.
 
     Args:
-        arr: the array to convert to a heat-map
-        cm: the color map to use (defaults to 'jet')
+        arr: the array to convert to an RGB heat-map
+        color_map: the color map to use (defaults to 'cubehelix')
+        -   cubehelix is like jet, but with a better luminosity gradient to
+            illustrate the scale to the human eye, as well as support black
+            and white printing (i.e., black and white cube helix will resemble
+            plt.cm.binary or plt.cm.Greys)
 
     Returns:
         arr converted to a heat-map via the CMRmap color map
@@ -17,7 +21,7 @@ def heatmap(arr, cm='jet'):
     # normalize the input data
     arr = plt.Normalize()(arr)
     # return the color map with the alpha channel omitted
-    heat = getattr(plt.cm, cm)(arr)[..., :-1] * 255
+    heat = getattr(plt.cm, color_map)(arr)[..., :-1] * 255
 
     return heat.astype('uint8')
 
