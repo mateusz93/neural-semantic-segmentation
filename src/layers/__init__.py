77396d1c25e881f4d16e14eedb552ac3d23a5dd9
Kautenja <ckfour@icloud.com>
2018-09-26 01:22:01 -0500

    remove static BN

--- src/layers/__init__.py
+++ src/layers/__init__.py
@@ -2,7 +2,6 @@
 from .contrast_normalization import ContrastNormalization
 from .memorized_pooling_2d import MemorizedMaxPooling2D
 from .memorized_upsampling_2d import MemorizedUpsampling2D
-from .static_batch_normalization import StaticBatchNormalization
 
 
 # explicitly define the outward facing API of this package
@@ -10,5 +9,4 @@ __all__ = [
     ContrastNormalization.__name__,
     MemorizedMaxPooling2D.__name__,
     MemorizedUpsampling2D.__name__,
-    StaticBatchNormalization.__name__,
 ]
