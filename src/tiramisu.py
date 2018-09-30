0510a4d1b158498a7bb7117a854bc974ac4cac42
Kautenja <ckfour@icloud.com>
2018-09-30 14:26:29 -0500

    update

--- src/tiramisu.py
+++ src/tiramisu.py
@@ -252,7 +252,7 @@ def build_tiramisu(image_shape: tuple, num_classes: int,
     # pass the logits through the Softmax activation to get probabilities
     softmax = Activation('softmax', name='softmax')(logits)
     # build the Tiramisu model
-    tiramisu = Model(inputs=[inputs], outputs=[softmax])
+    model = Model(inputs=[inputs], outputs=[softmax])
     # compile the model
     model.compile(
         optimizer=RMSprop(lr=learning_rate),
@@ -260,7 +260,7 @@ def build_tiramisu(image_shape: tuple, num_classes: int,
         metrics=metrics_for_segmentation(num_classes, label_names, class_weights),
     )
 
-    return tiramisu
+    return model
 
 
 def build_epistemic_tiramisu(image_shape: tuple, num_classes: int,
@@ -289,7 +289,7 @@ def build_epistemic_tiramisu(image_shape: tuple, num_classes: int,
         bottleneck_size: the number of convolutional layers in the bottleneck
         dropout: the dropout rate to use in dropout layers
         learning_rate: the learning rate for the RMSprop optimizer
-        samples: the number of samples for Monte Carlo estimation
+        samples: the number of samples for Monte Carlo integration
 
     Returns:
         a compiled model of the Tiramisu architecture + Epistemic
@@ -356,7 +356,7 @@ def build_aleatoric_tiramisu(image_shape: tuple, num_classes: int,
         bottleneck_size: the number of convolutional layers in the bottleneck
         dropout: the dropout rate to use in dropout layers
         learning_rate: the learning rate for the RMSprop optimizer
-        samples: the number of samples for Monte Carlo integration
+        samples: the number of samples for Monte Carlo loss estimation
 
     Returns:
         a compiled model of the Tiramisu architecture + Aleatoric
@@ -371,7 +371,7 @@ def build_aleatoric_tiramisu(image_shape: tuple, num_classes: int,
         dropout=dropout,
         split_head=True,
     )
-    # stack the Monte Carlo batches into a tensor along the 0th axis
+    # stack the logits and sigma for aleatoric loss
     aleatoric = Stack(name='aleatoric')([logits, sigma])
     # pass the logits through the Softmax activation to get probabilities
     softmax = Activation('softmax', name='softmax')(logits)
@@ -393,9 +393,85 @@ def build_aleatoric_tiramisu(image_shape: tuple, num_classes: int,
     return tiramisu
 
 
+def build_hybrid_tiramisu(image_shape: tuple, num_classes: int,
+    label_names: dict=None,
+    class_weights=None,
+    initial_filters: int=48,
+    growth_rate: int=16,
+    layer_sizes: list=[4, 5, 7, 10, 12],
+    bottleneck_size: int=15,
+    dropout: float=0.2,
+    learning_rate: float=1e-3,
+    aleatoric_samples: int=50,
+    epistemic_samples: int=50,
+):
+    """
+    Build a Tiramisu model that computes Aleatoric uncertainty.
+
+    Args:
+        image_shape: the image shape to create the model for
+        num_classes: the number of classes to segment for (e.g. c)
+        label_names: a dictionary mapping discrete labels to names for IoU
+        class_weights: the weights for each class
+        initial_filters: the number of filters in the first convolution layer
+        growth_rate: the growth rate to use for the network (e.g. k)
+        layer_sizes: a list with the size of each dense down-sample block.
+                     reversed to determine the size of the up-sample blocks
+        bottleneck_size: the number of convolutional layers in the bottleneck
+        dropout: the dropout rate to use in dropout layers
+        learning_rate: the learning rate for the RMSprop optimizer
+        aleatoric_samples: number of samples for Monte Carlo loss estimation
+        epistemic_samples: number of samples for Monte Carlo integration
+
+    Returns:
+        a compiled model of the Tiramisu architecture + Aleatoric
+
+    """
+    raise NotImplementedError
+    # # build the base of the network
+    # inputs, logits, sigma = _build_tiramisu(image_shape, num_classes,
+    #     initial_filters=initial_filters,
+    #     growth_rate=growth_rate,
+    #     layer_sizes=layer_sizes,
+    #     bottleneck_size=bottleneck_size,
+    #     dropout=dropout,
+    #     split_head=True,
+    # )
+    # # stack the logits and sigma for aleatoric loss
+    # aleatoric = Stack(name='aleatoric')([logits, sigma])
+    # # pass the logits through the Softmax activation to get probabilities
+    # softmax = Activation('softmax', name='softmax')(logits)
+    # # build the Tiramisu model
+    # tiramisu = Model(inputs=[inputs], outputs=[softmax, sigma, aleatoric])
+
+    # # the inputs for the Monte Carlo integration model
+    # inputs = Input(image_shape)
+    # # take the mean of Tiramisu output over the number of simulations
+    # mean = MonteCarloSimulation(tiramisu, epistemic_samples, name='mean')(inputs)
+    # # calculate the variance as the entropy of the means
+    # vari = Entropy(name='entropy')(mean)
+    # # build the epistemic uncertainty model
+    # model = Model(inputs=[inputs], outputs=[mean, vari])
+
+    # # compile the model
+    # model.compile(
+    #     optimizer=RMSprop(lr=learning_rate),
+    #     loss={
+    #         'mean': build_categorical_crossentropy(class_weights),
+    #         'aleatoric': build_categorical_aleatoric_loss(samples),
+    #     },
+    #     metrics={
+    #         'mean': metrics_for_segmentation(num_classes, label_names, class_weights)
+    #     },
+    # )
+
+    # return model
+
+
 # explicitly define the outward facing API of this module
 __all__ = [
     build_aleatoric_tiramisu.__name__,
     build_epistemic_tiramisu.__name__,
+    build_hybrid_tiramisu.__name__,
     build_tiramisu.__name__,
 ]
