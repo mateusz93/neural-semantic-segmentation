410d5e3441272601bad9fa4111f60ce3f3519f35
Kautenja <ckfour@icloud.com>
2018-10-01 21:59:13 -0500

    update utility methods

--- src/utils/extract_aleatoric.py
+++ src/utils/extract_aleatoric.py
@@ -1,25 +1,28 @@
-"""A method to extract the uncertainty for predicted class labels."""
+"""A method to extract the Aleatoric uncertainty for predicted class labels."""
 import numpy as np
 
 
-def extract_uncertainty(sigma: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+def extract_aleatoric(sigma, y_pred):
     """
-    Extract the uncertainty from sigma and predicted y outputs.
+    Extract the Aleatoric uncertainty from sigma and predicted y outputs.
 
     Args:
         sigma: the standard deviation predicted by the loss network
         y_pred: the predicted values as either a one-hot or Softmax output
 
     Returns:
-        a 2D batch tensor of uncertainties
+        a 2D batch tensor of aleatoric uncertainties
 
     """
     # use ogrid to index sigma using the integer class labels
     ogrid = np.ogrid[0:y_pred.shape[0], 0:y_pred.shape[1], 0:y_pred.shape[2]]
     batch, height, width = ogrid
     # use fancy indexing to extract the value at the index of each class label
-    return sigma[batch, height, width, np.argmax(y_pred, axis=-1)]
+    aleatoric = sigma[batch, height, width, np.argmax(y_pred, axis=-1)]
+    aleatoric = plt.Normalize()(aleatoric)
+
+    return aleatoric
 
 
 # explicitly define the outward facing API of this module
-__all__ = [extract_uncertainty.__name__]
+__all__ = [extract_aleatoric.__name__]
