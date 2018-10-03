50074ae4f9a23ed20acd1f219acc67f2819a3802
Kautenja <ckfour@icloud.com>
2018-10-02 19:31:58 -0500

    epistemic approximation

--- src/tiramisu.py
+++ src/tiramisu.py
@@ -14,6 +14,7 @@ from keras.optimizers import RMSprop
 from keras.regularizers import l2
 from .layers import Entropy
 from .layers import MonteCarloSimulation
+from .layers import MovingAverage
 from .layers import Stack
 from .losses import build_categorical_crossentropy
 from .losses import build_categorical_aleatoric_loss
@@ -307,16 +308,16 @@ def build_epistemic_tiramisu(image_shape: tuple, num_classes: int,
     # pass the logits through the Softmax activation to get probabilities
     softmax = Activation('softmax')(logits)
     # build the Tiramisu model
-    tiramisu = Model(inputs=[inputs], outputs=[softmax])
+    tiramisu = Model(inputs=[inputs], outputs=[softmax], name='tiramisu')
 
     # the inputs for the Monte Carlo model
     inputs = Input(image_shape)
     # take the mean of Tiramisu output over the number of simulations
     mean = MonteCarloSimulation(tiramisu, samples, name='mean')(inputs)
     # calculate the variance as the entropy of the means
-    vari = Entropy(name='entropy')(mean)
+    entropy = Entropy(name='entropy')(mean)
     # build the epistemic uncertainty model
-    model = Model(inputs=[inputs], outputs=[mean, vari])
+    model = Model(inputs=[inputs], outputs=[mean, entropy])
 
     # compile the model
     model.compile(
@@ -324,6 +325,76 @@ def build_epistemic_tiramisu(image_shape: tuple, num_classes: int,
         loss={'mean': build_categorical_crossentropy(class_weights)},
         metrics={
             'mean': metrics_for_segmentation(num_classes, label_names, class_weights)
+        },
+    )
+
+    return model
+
+
+def build_epi_approx_tiramisu(image_shape: tuple, num_classes: int,
+    label_names: dict=dict(),
+    class_weights=None,
+    initial_filters: int=48,
+    growth_rate: int=16,
+    layer_sizes: list=[4, 5, 7, 10, 12],
+    bottleneck_size: int=15,
+    dropout: float=0.2,
+    learning_rate: float=1e-3,
+    momentum: float=0.9,
+):
+    """
+    Build a Tiramisu model that estimates Epistemic uncertainty.
+
+    Args:
+        image_shape: the image shape to create the model for
+        num_classes: the number of classes to segment for (e.g. c)
+        label_names: a dictionary mapping discrete labels to names for IoU
+        class_weights: the weights for each class
+        initial_filters: the number of filters in the first convolution layer
+        growth_rate: the growth rate to use for the network (e.g. k)
+        layer_sizes: a list with the size of each dense down-sample block.
+                     reversed to determine the size of the up-sample blocks
+        bottleneck_size: the number of convolutional layers in the bottleneck
+        dropout: the dropout rate to use in dropout layers
+        learning_rate: the learning rate for the RMSprop optimizer
+        momentum: the momentum for the exponential moving average
+
+    Returns:
+        a compiled model of the Tiramisu architecture + Epistemic approximation
+
+    """
+    # build the base of the network
+    inputs, logits = _build_tiramisu(image_shape, num_classes,
+        initial_filters=initial_filters,
+        growth_rate=growth_rate,
+        layer_sizes=layer_sizes,
+        bottleneck_size=bottleneck_size,
+        dropout=dropout,
+        mc_dropout=True,
+    )
+    # pass the logits through the Softmax activation to get probabilities
+    softmax = Activation('softmax', name='softmax')(logits)
+    # build the Tiramisu model
+    tiramisu = Model(inputs=[inputs], outputs=[softmax, entropy])
+
+    # the inputs for the Monte Carlo model
+    inputs = Input(image_shape)
+    # pass the values through the Tiramisu network
+    tiramisu_out = tiramisu(inputs)
+    # create an exponential moving average of softmax to estimate a
+    # Monte Carlo simulation and provide epistemic uncertainty
+    mean = MovingAverage()(tiramisu_out)
+    # calculate the epistemic uncertainty as the entropy of the means
+    entropy = Entropy(name='entropy')(mean)
+    # build the epistemic uncertainty model
+    model = Model(inputs=[inputs], outputs=[tiramisu_out, entropy])
+
+    # compile the model
+    model.compile(
+        optimizer=RMSprop(lr=learning_rate),
+        loss={'softmax': build_categorical_crossentropy(class_weights)},
+        metrics={
+            'softmax': metrics_for_segmentation(num_classes, label_names, class_weights)
         },
     )
 
