6a519b60db5ff25c5a6be6d60c809cc6f6cc2704
Kautenja <kautencreations@gmail.com>
2018-10-03 17:31:56 -0500

    method to load mapping files

--- src/camvid/__init__.py
+++ src/camvid/__init__.py
@@ -1,10 +1,12 @@
 """This package provides methods to load the CamVid dataset into memory."""
 from .camvid import CamVid
+from .load_mapping import load_mapping
 from .plot import plot
 
 
 # explicitly define the outward facing API of this package
 __all__ = [
     CamVid.__name__,
+    load_mapping.__name__,
     plot.__name__,
 ]
