cd1b31578134a93a291b6b510121399adc9ec9ca
Kautenja <kautencreations@gmail.com>
2018-10-03 23:09:49 -0500

    fix issues

--- src/predict_video.py
+++ src/predict_video.py
@@ -29,27 +29,26 @@ def predict_video(
     # create a stream to read the input video
     reader = imageio.get_reader(video_path)
     # the shape to resize frames to
-    # image_shape = (352, 480, 3)
-    image_shape = model.input_shape
+    image_shape = model.input_shape[1:]
     # create a video writer with source FPS
     writer = imageio.get_writer(out_path, fps=reader.get_meta_data()['fps'])
 
     # iterate over the frames in the source video
     for frame in tqdm(reader):
         # resize the image to the acceptable size for the model
-        X = transform.resize(frame, image_shape,
+        img = transform.resize(frame, image_shape,
             anti_aliasing=False,
             mode='symmetric',
             clip=False,
             preserve_range=True,
-        )
+        )[None, ...]
         # predict mean and model variance of the frame
-        outs = predict(model, X, camvid)
-        h, w, c = image_shape
+        outs = predict(model, img, camvid)
+        height, width, channels = image_shape
         # convert the three images into a singular image (side-by-side)
-        image = np.zeros((h, w * len(outs), c), dtype='uint8')
+        image = np.zeros((height, width * len(outs), channels), dtype='uint8')
         for idx, piece in enumerate(outs):
-            image[:, idx*w:(idx+1)*w, :] = piece
+            image[:, idx * width:(idx + 1) * width, :] = piece
         # save the image to the stream
         writer.append_data(image)
 
