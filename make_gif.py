96276f08d10cdd50176f6c4964310f0fe21d6a30
Kautenja <ckfour@icloud.com>
2018-10-05 00:33:22 -0500

    cleanup

--- make_gif.py
+++ make_gif.py
@@ -1,7 +1,28 @@
-"""A simple script to turn an MP4 file into a GIF file."""
+"""Convert an MP4 file to a GIF file.
+
+python make_gif.py <mp4 file>
+
+"""
 import sys
 from moviepy.editor import VideoFileClip
 
 
-clip = VideoFileClip(sys.argv[1]).subclip((0, 50), (1, 10)).set_fps(5).resize(0.5)
-clip.write_gif(sys.argv[1].replace('.mp4', '.gif'))
+try:
+    # try to get the video file from the command line
+    video_file = sys.argv[1]
+except IndexError:
+    # invalid input args, print documentation and exit
+    print(__doc__)
+    sys.exit(-1)
+
+
+# load the input clip
+video = VideoFileClip(video_file)
+# clip the video into a range of ~20s
+video = video.subclip((0, 50), (1, 10))
+# use a small frame rate to keep file size low
+video = video.set_fps(5)
+# downscale the video by 2x to keep file size low
+video = video.resize(0.5)
+# write the clip as a GIF
+video.write_gif(video_file.replace('.mp4', '.gif'))
